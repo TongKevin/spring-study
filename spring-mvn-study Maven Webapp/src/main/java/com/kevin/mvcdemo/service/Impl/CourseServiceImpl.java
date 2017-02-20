e8ab547ac41fd569921e2c8dd4ce28e705c50133
Kevin.Tong <tmtongming@163.com>
2017-02-20 23:13:11 +0800

    the more

--- spring-mvn-study Maven Webapp/src/main/java/com/kevin/mvcdemo/service/Impl/CourseServiceImpl.java
+++ spring-mvn-study Maven Webapp/src/main/java/com/kevin/mvcdemo/service/Impl/CourseServiceImpl.java
@@ -1,16 +1,23 @@
 package com.kevin.mvcdemo.service.Impl;
 
+import org.springframework.stereotype.Service;
+
 import com.kevin.mvcdemo.model.Course;
 import com.kevin.mvcdemo.service.CourseService;
 
+
+@Service("courseService")
 public class CourseServiceImpl implements CourseService
 {
 	
 	@Override
 	public Course getCourseById(Integer courseId)
 	{
-		// TODO Auto-generated method stub
-		return null;
+
+		Course course = new Course();
+		course.setCourseId(courseId);
+		course.setTitle("course1");
+		return course;
 	}
 	
 }
